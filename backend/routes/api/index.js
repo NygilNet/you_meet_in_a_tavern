18e66517d777de440d972ec4bb6f84e79d9dce59
NygilNet <nygilnet@gmail.com>
2023-01-23 11:25:54 -0600

    FEAT: add user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 const { restoreUser } = require('../../utils/auth');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 
 module.exports = router;
