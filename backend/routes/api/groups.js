06c225ce3acb09846ef1c52a3a6f629e70ebbbcc
NygilNet <nygilnet@gmail.com>
2023-01-24 15:09:13 -0600

    FEAT: added groups endpoints that find all groups related to current user and finding a group by the group"s id

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { Group, GroupImage, Membership } = require('../../db/models');
+const { Group, GroupImage, Membership, Venue, User } = require('../../db/models');
 const { Op } = require('sequelize');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
@@ -44,6 +45,105 @@ router.get('/', async (req, res) => {
     res.json(groupsList);
 });
 
+// GET ALL GROUPS JOINED OR ORGANIZED BY THE CURRENT USER
+router.get('/current', requireAuth, async (req, res) => {
+
+    const userId = req.user.id;
+
+    const groups = await Group.findAll({
+        include: [
+            {
+                model: Membership,
+
+            },
+            {
+                model: GroupImage
+
+            }
+        ]
+    });
+
+    const userGroups = [];
+
+    groups.forEach(group => {
+        const grp = group.toJSON();
+
+        if (grp.organizerId === userId) {
+            userGroups.push(grp);
+            grp.inArray = true;
+        }
+
+        grp.Memberships.forEach (member => {
+            if (member.userId === userId && !grp.inArray) {
+                userGroups.push(grp);
+                grp.inArray = true;
+            }
+        })
+    });
+
+    userGroups.forEach(group => {
+
+        let count = 0;
+        const nonMems = ['pending', 'waitlist'];
+        group.Memberships.forEach(member => {
+            if (! nonMems.includes(member.status)) count++;
+        })
+        group.numMembers = count;
+        delete group.Memberships;
+
+        group.GroupImages.forEach(img => {
+            if (img.preview === true) group.previewImage = img.url;
+        });
+        if (!group.previewImage) groupsList.previewImage = 'no preview image provided';
+        delete group.GroupImages;
+
+        delete group.inArray;
+    });
+
+    res.json(userGroups);
+});
+
+// GET DETAILS OF A GROUP FROM AN ID
+router.get('/:groupId', async (req, res) => {
+    const id = req.params.groupId;
+
+    const group = await Group.findByPk(id, {
+        include: [
+            {
+                model: Membership
+            },
+            {
+                model: GroupImage
+            },
+            {
+                model: Venue,
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            }
+        ]
+    });
+
+    if (!group) return res.status(404).json({
+        message: `No group with id ${id} found.`
+    })
+
+    const detailed = group.toJSON();
+
+    let count = 0;
+    const nonMems = ['pending', 'waitlist'];
+    detailed.Memberships.forEach(member => {
+        if (! nonMems.includes(member.status)) count++;
+    });
+    detailed.numMembers = count;
+    delete detailed.Memberships;
+
+    detailed.Organizer = await User.findByPk(detailed.organizerId, {
+        attributes: ['id', 'firstName', 'lastName']
+    });
+
+    res.json(detailed);
+});
 
 
 module.exports = router;
