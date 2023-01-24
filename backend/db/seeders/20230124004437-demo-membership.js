4ed1dcd70fcbbdc3bf646eef944292734e490f36
NygilNet <nygilnet@gmail.com>
2023-01-23 18:53:28 -0600

    UPD: added seeder file for Memberships table

---
+++ backend/db/seeders/20230124004437-demo-membership.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   options.tableName = 'Memberships';
+   return queryInterface.bulkInsert(options, [
+    {
+      userId: 1,
+      groupId: 1,
+      status: 'co-host'
+    },
+    {
+      userId: 3,
+      groupId: 2,
+      status: 'co-host'
+    },
+    {
+      userId: 2,
+      groupId: 3,
+      status: 'co-host'
+    },
+    {
+      userId: 2,
+      groupId: 1,
+      status: 'pending'
+    }
+   ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Memberships';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      status: { [Op.in]: ['co-host', 'member', 'pending'] }
+    })
+  }
+};
