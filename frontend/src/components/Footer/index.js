2eb9bab4678c95b0730f1381556f03c59012e90e
NygilNet <nygilnet@gmail.com>
2023-04-24 15:09:05 -0500

    FIX: github repo link now leads to correct repo

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -10,7 +10,7 @@ function Footer() {
                 <ul>
                 <li><a href="https://github.com/NygilNet" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github fa-2x" style={{color: "#CCCCCC"}} ></i></a></li>
                 <li><a href="https://www.linkedin.com/in/nygil-nettles-dev/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin fa-2x" style={{color: "#CCCCCC"}} ></i></a></li>
-                <li><a href="https://github.com/NygilNet/aa-capstone-project" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-book-bookmark fa-2x" style={{color: "#CCCCCC"}} ></i></a></li>
+                <li><a href="https://github.com/NygilNet/you_meet_in_a_tavern" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-book-bookmark fa-2x" style={{color: "#CCCCCC"}} ></i></a></li>
                 </ul>
             </div>
             <div className="footer-copyright">
